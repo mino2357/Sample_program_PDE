b1fc5f26d7b79e1f9b9d1039d0f35730b0f17011
mino2357 <mino2357@gmail.com>
2017-09-12 14:59:49 +0900

    バグ修正

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <Eigen/Sparse>
 //#include <Eigen/Dense>
 
-constexpr int N = 1000;
+constexpr int N = 1024;
 constexpr double L = 1.;
 constexpr double dx = L / (N + 1);
 constexpr double xStart = 0 + dx;
@@ -22,7 +22,7 @@ constexpr T func(const T& x) noexcept{
 
 template <typename T = double>
 constexpr T exactSol(const T& x) noexcept{
-    return std::sin(x) * (boundaryB / std::sin(L) - boundaryA /std::tan(L)) + boundaryA * std::cos(x);
+    return - 1. / 6. * x * (x * x - 7.);
 }
 
 int main(){
@@ -33,15 +33,15 @@ int main(){
     VectorNd b;
     VectorNd x;
 
-    for(int i{}; i<N; ++i){
+    for(int i=0; i<N; ++i){
         A.insert(i, i) = - 2.;
         b(i) = dx * dx * func<>(xStart + i * dx);
     }
 
     b(0)   = dx * dx * func<>(0) - boundaryA;
     b(N-1) = dx * dx * func<>(L) - boundaryB;
-    
-    for(int i{}; i<N-1; ++i){
+
+    for(int i=0; i<N-1; ++i){
         A.insert(i, i+1) = 1.;
         A.insert(i+1, i) = 1.;
     }
@@ -55,10 +55,19 @@ int main(){
     solver.factorize(A);
 
     x = solver.solve(b);
-
+    
+    
     std::cout << xStart - dx << " " << boundaryA - exactSol<>(xStart - dx) << std::endl;
     for(int i{}; i<N; ++i){
         std::cout << xStart + i * dx << " " << x(i) - exactSol<>(xStart + i * dx) << std::endl;
     }
     std::cout << xEnd + dx << " " << boundaryB - exactSol<>(xEnd + dx) << std::endl;
+    
+/*
+    std::cout << xStart - dx << " " << boundaryA << std::endl;
+    for(int i{}; i<N; ++i){
+        std::cout << xStart + i * dx << " " << x(i) << std::endl;
+    }
+    std::cout << xEnd + dx << " " << boundaryB << std::endl;
+*/
 }
