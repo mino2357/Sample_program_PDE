ce9ca2d7c5a43cc50b6477f63dd07a3f2b8c67cf
mino2357 <mino2357@gmail.com>
2017-12-30 21:00:05 +0900

    add fix

--- Sample001/main.cpp
+++ Sample001/main.cpp
@@ -2,36 +2,56 @@
  * 拡散方程式を解く．
  *
  * Takaaki MINOMO
+ * 私のコメントには先頭に mino) がつきます
+ *
+ * 改悪:Rittai_3D（仮名）
+ *  わたしのコメントには先頭に Rittai) がつきます
  */
 
 #include <iostream>
+#include <array>
 #include <cmath>
 
-
-//template<typename T = double>
-//constexpr T pi = 3.14159265358979323846264338327;
+// Rittai)個人的に、定数には static もつけます.
+// mino)そうします．
 
 //区間をN等分する．
-const int N = 100;
+static const int N = 100;
 //円周率
-const double pi = 3.14159265358979323846264338327;
+// Rittai)ちなみに <cmath> に M_PI ってのがあります
+static const double pi = acos(-1.);
 //区間
-const double L = pi;
+static const double L = pi;
+//境界条件
+static const double a = 0.;
+static const double b = 0.;
 
-int main(){
-
-    auto *u = new double[N+1];
-
+int main()
+{
+    //分割した微小区間幅
     double dx = L / N;
+    //スタート地点（空間方向）
     double x  = 0.;
-    
-    for(int i=0; i<=N; ++i){
-        u[i] = std::sin(x + i*dx);
+	
+    // Rittai)配列より std::array. range-based for に index が欲しい！
+	//        元コードの疑問点：普通の配列で十分なのに new double[ N + 1 ] するのは何故？
+	// mino)
+    //        配列と言えばそれしか思い浮かばなかった．std::array使います．
+    std::array< double, N + 1 > u;
+	
+    //初期条件   
+    for( int i=0 ; i<=N ; ++i ) {
+        u[i] = std::sin( x + i * dx );
     }
 
-    for(int i=0; i<=N; ++i){
-        std::cout << x + i*dx << " " << u[i] << std::endl;
-    }
+    //境界条件
+    u[0] = a;
+    u[N] = b;
 
-    delete[] u;
+
+    //デバッグ用出力
+    for( int i=0 ; i<=N ; ++i ) {
+        std::cout << x + i * dx << " " << u[i] << std::endl;
+    }
 }
+
